6ed6bedae86597a68b6c0fe50183b1020e3a9417
Людмила Ш. Боташева <botasheva.lyudmila@rus.local>
2022-10-06 13:54:53 +0300

    10 задача

---
+++ 10Example/Program.cs
@@ -0,0 +1,9 @@
+﻿//задача10
+Console.WriteLine("Введите трехзанчное число");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 100|n > 999) 
+{
+Console.WriteLine("Вы ввели недопустимое число, повторите ввод:");
+n = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("Вторая цифра числа " + Convert.ToString(n/10%10));
