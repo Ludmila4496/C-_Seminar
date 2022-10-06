45434169aa2ac7c2e8d6b2f525e24a801aca1cd3
Людмила Ш. Боташева <botasheva.lyudmila@rus.local>
2022-10-06 14:01:05 +0300

    15 задача

---
+++ 15Example/Program.cs
@@ -0,0 +1,10 @@
+﻿//задача15
+Console.WriteLine("Введите день недели от 1 до 7");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 | n > 7) 
+{
+Console.WriteLine("В неделе 7 дней, введите заново:");
+n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == 6| n == 7) Console.Write("Ура выходной!!!");
+else Console.WriteLine("Пашем негры, солнце еще высоко");
