eaad82c2bcf176eae3cb6e70fd0a74b24e9f5b64
Людмила Ш. Боташева <botasheva.lyudmila@rus.local>
2022-10-06 13:38:25 +0300

    2 задача

---
+++ 2Example/Program.cs
@@ -0,0 +1,8 @@
+﻿//Задача2
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA > numberB) Console.WriteLine(numberA + " больше чем " + numberB);
+else if (numberA == numberB) Console.WriteLine("Числа равны");
+else Console.WriteLine(numberB + " больше чем " + numberA);
