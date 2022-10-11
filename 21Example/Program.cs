4cf9025e2b9110f622dd4a086c65571a95251b8b
Людмила Ш. Боташева <botasheva.lyudmila@rus.local>
2022-10-11 16:17:54 +0300

    21 задача

---
+++ 21Example/Program.cs
@@ -0,0 +1,18 @@
+﻿//задача21
+Console.WriteLine("Введите координаты точки A");
+int xa = Convert.ToInt32(Console.ReadLine());
+int ya = Convert.ToInt32(Console.ReadLine());
+int za = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B");
+int xb = Convert.ToInt32(Console.ReadLine());
+int yb = Convert.ToInt32(Console.ReadLine());
+int zb = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Round(Distance(xa, ya, za, xb, yb, zb), 2, MidpointRounding.ToZero);
+Console.WriteLine($"Расстояние от точки А до точки В в 3D пространстве: {distance}");
+
+double Distance (int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double dist = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2)+ Math.Pow((z2-z1), 2));
+    return dist;
+}
