345401414c3c67aa7dbc4c5d93b01cb39bfa1076
Людмила Ш. Боташева <botasheva.lyudmila@rus.local>
2022-10-17 16:57:02 +0300

    Задача 38

---
+++ 38Example/Program.cs
@@ -0,0 +1,52 @@
+﻿double[] CreateArrayRndDouble(int size, int min, int max)
+{
+    var array = new double[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        double rndNum = rnd.NextDouble() * 100;
+        array[i] = Math.Round(rndNum, 2);
+    }
+    return array;
+}
+
+void PrintArrayDouble(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}; ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+double MaxElement(double[] array)
+{
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+            max = array[i];
+    }
+    return max;
+}
+
+double MinElement(double[] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+            min = array[i];
+    }
+    return min;
+}
+
+
+double[] arr = CreateArrayRndDouble(6, 0, 100);
+PrintArrayDouble(arr);
+
+double subtraction = Math.Round(MaxElement(arr) - MinElement(arr), 2);
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива = {subtraction}");
