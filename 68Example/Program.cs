993c7372663c03172a396c049970af4f8fb17fca
Людмила Ш. Боташева <botasheva.lyudmila@rus.local>
2022-11-16 13:50:13 +0300

    Итоговый проект Задача 68

---
+++ 68Example/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+Console.Write("Введите число m: ");
+int m = CorrectNaturalNumber();
+
+Console.Write("Введите число n: ");
+int n = CorrectNaturalNumber();
+
+AkkermanFunction(m,n);
+
+// вызов функции Аккермана
+void AkkermanFunction(int m, int n)
+{
+    Console.Write(Akkerman(m, n)); 
+}
+
+// функция Аккермана
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
+
+int CorrectNaturalNumber()                                       //метод проверки размерности массива
+{
+    string value="";
+    int number=0;
+    while (true)
+    {
+        value=Console.ReadLine();
+        try 
+        {
+            number=int.Parse(value);
+        }
+        catch
+        {
+            Console.Write ("ОШИБКА! Вы ввели не натуральное число, повторите ввод: ");
+            continue;
+        }
+        if (number<1) 
+        {
+            Console.Write ("ОШИБКА! Вы ввели число меньше 1, повторите ввод: ");
+            continue;
+        }
+        return number;
+    }
+} 
